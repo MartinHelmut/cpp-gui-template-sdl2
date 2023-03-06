8447620a3525749e072efe503b526f5c59de899f
Martin Helmut Fieber <info@martin-fieber.se>
2023-03-06 21:28:56 +0100

    Base setup of app packaging

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,14 +1,14 @@
 if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
   find_program(CLANGTIDY clang-tidy)
   if (CLANGTIDY)
-    message(STATUS "Using clang-tidy")
+    message(STATUS "Using clang-tidy, found ${CLANGTIDY}")
     set(CMAKE_CXX_CLANG_TIDY "${CLANGTIDY};-extra-arg=-Wno-unknown-warning-option")
   else ()
     message(WARNING "clang-tidy requested but executable not found")
   endif ()
 
   # This will gradually increase memory usage of the program,
-  # running on Apple M1, 13.0.
+  # discovered on Apple M1, 13.0.
   if (NOT WIN32)
     message(STATUS "Using address sanitizer")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -fsanitize=address -g")
--- cmake/UniversalAppleBuild.cmake
+++ cmake/UniversalAppleBuild.cmake
@@ -1,5 +1,8 @@
 # Generate universal executable for Apple hardware
 # This file needs to be included before calling `project`.
 if (APPLE)
-  set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64" CACHE STRING "")
+  # Universal build with "arm64;x86_64"
+  # Currently deactivated due to an issue with CMake and clang-tidy.
+  # @see: https://gitlab.kitware.com/cmake/cmake/-/issues/24323
+  set(CMAKE_OSX_ARCHITECTURES "${ARCHS_STANDARD}" CACHE STRING "")
 endif ()
