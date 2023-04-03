d21a6b023b8aaf64ec12a92b56c5374b72ed7a5f
Martin Helmut Fieber <info@martin-fieber.se>
2023-04-03 21:17:15 +0200

    Add test setup with separated runner and test libraries

---
+++ src/core/Tests/Resources.spec.cpp
@@ -0,0 +1,8 @@
+#include <doctest/doctest.h>
+
+TEST_SUITE("Core::Resources") {
+  TEST_CASE("Example") {
+    const std::string input{"A"};
+    CHECK_EQ(input, "A");
+  }
+}
---
+++ src/tests/TestRunner.cpp
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2023 Martin Helmut Fieber <info@martin-fieber.se>
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
