da8f611a470f86e1490c1c63ba6032f25d3f2389
Martin Helmut Fieber <info@martin-fieber.se>
2023-04-17 20:27:25 +0200

    Reactivate clang-tidy

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -41,7 +41,7 @@ function(set_project_warnings project_name)
   set(CLANG_WARNINGS
     -Wall
     -Wextra # reasonable and standard
-    # -Wshadow # warn the user if a variable declaration shadows one from a
+    -Wshadow # warn the user if a variable declaration shadows one from a
     # parent context
     -Wnon-virtual-dtor # warn the user if a class with virtual functions has a
     # non-virtual destructor. This helps catch hard to
