7a8858a76781b5ba58441d2a526e2192b3b8425a
Martin Helmut Fieber <info@martin-fieber.se>
2023-04-28 12:59:08 +0200

    Add RelWithDebInfo as CMake target

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -4,7 +4,7 @@ if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
 
   # Set the possible values of build type for cmake-gui, ccmake
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release")
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
 endif ()
 
 find_program(CCACHE ccache)
