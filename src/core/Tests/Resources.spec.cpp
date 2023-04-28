9617d23a9a48d1b5ea12c023c06291b23e06d19b
Martin Helmut Fieber <info@martin-fieber.se>
2023-04-28 12:58:17 +0200

    Fix clang-tidy warnings for doctest

--- src/core/Tests/Resources.spec.cpp
+++ src/core/Tests/Resources.spec.cpp
@@ -1,9 +1,13 @@
 #include <doctest/doctest.h>
 #include <string>
 
+// NOLINTBEGIN(misc-use-anonymous-namespace, cppcoreguidelines-avoid-do-while, cert-err33-c)
+
 TEST_SUITE("Core::Resources") {
   TEST_CASE("Example") {
     const std::string input{"A"};
     CHECK_EQ(input, "A");
   }
 }
+
+// NOLINTEND(misc-use-anonymous-namespace, cppcoreguidelines-avoid-do-while, cert-err33-c)
