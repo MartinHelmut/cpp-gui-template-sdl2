6b9269b663682c584b614aded1da19cde86ff017
Martin Helmut Fieber <info@martin-fieber.se>
2023-09-21 15:00:48 +0200

    Integrate CEF as vendor package

--- src/app/App/Main.cpp
+++ src/app/App/Main.cpp
@@ -2,6 +2,10 @@
 #include "Core/Debug/Instrumentor.hpp"
 #include "Core/Log.hpp"
 
+// @info: WORKS!
+#include <include/cef_browser.h>
+#include <include/cef_app.h>
+
 int main() {
   try {
     APP_PROFILE_BEGIN_SESSION_WITH_FILE("App", "profile.json");
