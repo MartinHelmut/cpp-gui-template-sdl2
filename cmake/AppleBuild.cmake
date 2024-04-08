df7e543c2c086b258e37026665e0504780f0ffbd
Martin Helmut Fieber <info@martin-fieber.se>
2024-04-08 23:03:37 +0200

    Better Apple build settings

--- cmake/AppleBuild.cmake
+++ cmake/AppleBuild.cmake
@@ -1,6 +1,9 @@
-# Define apple architecture for Release builds, use default. For an explicit
-# universal executable use `x86_64;arm64`.
 # This file needs to be included before calling `project`.
 if (APPLE AND "${CMAKE_GENERATOR}" STREQUAL "Xcode")
-  set(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)" CACHE INTERNAL "")
+  # Define apple architecture for Release builds, use default. For an explicit
+  # universal executable use `x86_64;arm64`.
+  set(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)" CACHE INTERNAL "OS X architecture")
+
+  # Support older macOS versions.
+  set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15 CACHE STRING "Minimum OS X deployment version")
 endif ()
