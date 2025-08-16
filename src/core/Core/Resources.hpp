0a710f7d13a2a5a63bfb6ee973e469daf69f1fb8
Martin Helmut Fieber <info@martin-fieber.se>
2025-08-16 21:15:05 +0300

    Make font loading optional

--- src/core/Core/Resources.hpp
+++ src/core/Core/Resources.hpp
@@ -9,6 +9,7 @@ class Resources {
  public:
   [[nodiscard]] static std::filesystem::path resource_path(const std::filesystem::path& file_path);
   [[nodiscard]] static std::filesystem::path font_path(const std::string_view& font_file);
+  [[nodiscard]] static bool exists(const std::filesystem::path& pathname);
 };
 
 }  // namespace App
